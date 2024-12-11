ac859355fca8c2102499c497e9c5c3a36ba4cb24
jackkester <jwkester@syr.edu>
2024-12-10 20:09:07 -0500

    tully_scraper commit

--- code/tully_scraper.py
+++ code/tully_scraper.py
@@ -10,9 +10,21 @@ def tullyscraper(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.tullysgoodtimes.com/menus/")
 
-    # TODO Write code here
+    list_of_dicts = []
+    for title in page.query_selector_all("h3.foodmenu__menu-section-title"):
+        title_text = title.inner_text()
+        print("MENU SECTION:", title_text)
+        row = title.query_selector("~ *").query_selector("~ *")
+
+        for item in row.query_selector_all("div.foodmenu__menu-item"):
+            item_text = item.inner_text()
+            extracted_item = extract_menu_item(title_text, item_text)
+            list_of_dicts.append(extracted_item.to_dict())
+            print(f"  MENU ITEM: {extracted_item.name}")
+            
+    df = pd.DataFrame(list_of_dicts)
+    df.to_csv("cache/tullys_menu.csv", index=False)
     
-    # ---------------------
     context.close()
     browser.close()
 
